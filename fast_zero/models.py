a999c9e81701aad29bd158fecc57f2def2cc7886
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-09-29 16:40:16 -0300

    feat(issue-5): Integrated Database wit API.

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from pydantic import BaseModel, ConfigDict, EmailStr
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -14,3 +13,10 @@ class User(Base):
     username: Mapped[str]
     password: Mapped[str]
     email: Mapped[str]
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
