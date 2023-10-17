ce3f9fa6bc885170b17e872f52f66ca5f4784fb7
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-10-17 00:20:25 -0300

    Update settings.py

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8'
+        _env_file='.env', _env_file_encoding='utf-8'
     )
 
     DATABASE_URL: str
