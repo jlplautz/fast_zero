4fb81538aaf5ebd11b1aa515d6a8a4ad6e68039c
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-10-17 14:22:51 -0300

    Update settings.py

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -1,10 +1,10 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings#, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8'
-    )
+    # model_config = SettingsConfigDict(
+    #     env_file='.env', env_file_encoding='utf-8'
+    # )
 
     DATABASE_URL: str = 'sqlite:///database.db'
     SECRET_KEY: str = 'your-secret-key'
